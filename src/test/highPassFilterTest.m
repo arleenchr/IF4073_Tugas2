9d887877c8bf1b86138404540d43ee7b5ca19e9d
Arleen <13521059@mahasiswa.itb.ac.id>
2024-10-26 00:04:47 +0700

    fix: test add all cases

--- src/test/highPassFilterTest.m
+++ src/test/highPassFilterTest.m
@@ -1,5 +1,7 @@
 image = imread('images/3-2.jpg');
 imshow(image);
 
-imageOutput = highPassFilter(image,'Butterworth', 50, 2);
+imageOutput = highPassFilter(image,'Gaussian', 50);
+%imageOutput = highPassFilter(image,'Ideal', 50);
+%imageOutput = highPassFilter(image,'Butterworth', 50, 2);
 figure; imshow(imageOutput);
--- src/test/lowPassFilterTest.m
+++ src/test/lowPassFilterTest.m
@@ -1,6 +1,8 @@
 image = imread('images/2-1.jpg');
 imshow(image);
 
-imageOutput = lowPassFilter(image,'Butterworth', 50, 2);
+imageOutput = lowPassFilter(image,'Ideal', 50);
+%imageOutput = lowPassFilter(image,'Gaussian', 50);
+%imageOutput = lowPassFilter(image,'Butterworth', 50, 2);
 figure; 
 imshow(imageOutput);
